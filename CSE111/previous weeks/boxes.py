435672871c187ccb1c9f1bce5ded3f1d580848bd
ascogeno <ros21045@byui.edu>
2025-01-10 12:26:06 -0700

    cleaning up house

--- CSE111/previous weeks/boxes.py
+++
@@ -1,8 +0,0 @@
-import math
-#gets user input for the number of items and boxes
-items = int(input("Enter the number of items: "))
-per_b = int(input("Enter the number of items PER BOX: "))
-#does the math for the boxes
-num_boxes = math.ceil(items/per_b)
-#output
-print(f"For {items} items, packing {per_b} in each box, you will need {num_boxes} boxes.")
--- CSE111/previous weeks/time_practice.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def print_time_and_message(message):
-    print(message)
-    print(datetime.now())
-
-print_time_and_message("Hello world! Here is the current time.")
