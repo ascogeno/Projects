5a492a924570f4b893ec0ce0533de67a19e0f1c3
ascogeno <ros21045@byui.edu>
2025-02-27 13:18:52 -0700

    buncha stuff done

--- wdd131/courses/courses.js
+++ wdd131/courses/courses.js
@@ -7,7 +7,20 @@ const aCourse = {
         { sectionNum: 2, roomNum: "STC 373", enrolled: 28, days: 31, instructor: "Kermit T. Frog" }
     ],
     enrollStudent: function (sectionNum) {
-        const sectionIndex = this.sections.findIndex(sectionNum);
+        const sectionIndex = this.sections.findIndex((section) => section.sectionNum == sectionNum);
+
+        if (sectionIndex >= 0) {
+            this.sections[sectionIndex].enrolled++;
+        }
+        renderSections(this.sections);
+    },
+    dropStudent: function (sectionNum) {
+        const sectionIndex = this.sections.findIndex((section) => section.sectionNum == sectionNum);
+
+        if (sectionIndex >= 0) {
+            this.sections[sectionIndex].enrolled--;
+        }
+        renderSections(this.sections);
     },
 };
 
@@ -29,6 +42,16 @@ function renderSections(sections) {
     const html = sections.map(sectionTemplate);
     document.querySelector("#sections").innerHTML = html.join("");
 }
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.enrollStudent(sectionNum);
+})
+
+document.querySelector("#dropStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.dropStudent(sectionNum);
+})
 
 renderCourseDetails(aCourse);
 
